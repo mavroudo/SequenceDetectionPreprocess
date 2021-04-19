9dcb74f73ea6eb87ab8ee872f627c6ad56933d90
mavroudo <mavroudo@csd.auth.gr>
2021-04-19 14:02:33 +0300

    finished preprocess

---
+++ src/main/scala/auth/datalab/sequenceDetection/SetContainment/CassandraSetContainment.scala
@@ -0,0 +1,153 @@
+package auth.datalab.sequenceDetection.SetContainment
+
+
+import auth.datalab.sequenceDetection.SetContainment.SetContainment.SetCInverted
+import auth.datalab.sequenceDetection.{Structs, Utils}
+import com.datastax.driver.core.ConsistencyLevel
+import com.datastax.driver.core.querybuilder.QueryBuilder.append
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+import com.datastax.spark.connector.writer.WriteConf
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+class CassandraSetContainment {
+
+  private var cassandra_host: String = null
+  private var cassandra_port: String = null
+  private var cassandra_user: String = null
+  private var cassandra_pass: String = null
+  private var cassandra_replication_class: String = null
+  private var cassandra_replication_rack: String = null
+  private var cassandra_replication_factor: String = null
+  private var cassandra_keyspace_name: String = null
+  private var cassandra_write_consistency_level: String = null
+  private var cassandra_gc_grace_seconds: String = null
+  private var _configuration: SparkConf = null
+
+  def startSpark(): Unit = {
+    try {
+      cassandra_host = Utils.readEnvVariable("cassandra_host")
+      cassandra_port = Utils.readEnvVariable("cassandra_port")
+      cassandra_user = Utils.readEnvVariable("cassandra_user")
+      cassandra_pass = Utils.readEnvVariable("cassandra_pass")
+      cassandra_gc_grace_seconds = Utils.readEnvVariable("cassandra_gc_grace_seconds")
+      cassandra_keyspace_name = Utils.readEnvVariable("cassandra_keyspace_name_set") // we use different variable
+      cassandra_replication_class = Utils.readEnvVariable("cassandra_replication_class")
+      cassandra_replication_rack = Utils.readEnvVariable("cassandra_replication_rack")
+      cassandra_replication_factor = Utils.readEnvVariable("cassandra_replication_factor")
+      cassandra_write_consistency_level = Utils.readEnvVariable("cassandra_write_consistency_level")
+
+
+      println(cassandra_host, cassandra_keyspace_name, cassandra_keyspace_name)
+    } catch {
+      case e: NullPointerException =>
+        e.printStackTrace()
+        System.exit(1)
+    }
+    _configuration = new SparkConf()
+      .setAppName("FA Indexing")
+      .setMaster("local[*]")
+      .set("spark.cassandra.connection.host", cassandra_host)
+      .set("spark.cassandra.auth.username", cassandra_user)
+      .set("spark.cassandra.auth.password", cassandra_pass)
+      .set("spark.cassandra.connection.port", cassandra_port)
+      .set("spark.cassandra.output.consistency.level", cassandra_write_consistency_level)
+
+
+    val spark = SparkSession.builder().config(_configuration).getOrCreate()
+    println(s"Starting Spark version ${spark.version}")
+    try {
+      CassandraConnector(spark.sparkContext.getConf).withSessionDo { session =>
+        session.execute("create keyspace if not exists " + cassandra_keyspace_name + " WITH replication = "
+          + "{'class':'" + cassandra_replication_class + "', '" + cassandra_replication_rack + "':" + cassandra_replication_factor + "}")
+        session.execute("USE " + cassandra_keyspace_name + ";")
+      }
+    } catch {
+      case e: Exception =>
+        System.out.println("A problem occurred creating the keyspace")
+        print(e.printStackTrace())
+        //Stop Spark
+        spark.close()
+        System.exit(1)
+    }
+
+  }
+
+  def createTable(logName: String): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    val table_idx = logName + "_idx"
+    val structure = "event1_name text, event2_name text, sequences list<text>, PRIMARY KEY (event1_name, event2_name)"
+
+    try {
+      CassandraConnector(spark.sparkContext.getConf).withSessionDo { session =>
+        session.execute("CREATE TABLE IF NOT EXISTS " + cassandra_keyspace_name + "." +
+          table_idx + " (" + structure + ") " +
+          "WITH GC_GRACE_SECONDS=" + cassandra_gc_grace_seconds +
+          ";")
+
+      }
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        System.out.println("A problem occurred creating the table")
+        //Stop Spark
+        spark.close()
+        System.exit(1)
+    }
+  }
+
+  def dropTable(logName: String): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    val table_idx = logName + "_idx"
+    try {
+      CassandraConnector(spark.sparkContext.getConf).withSessionDo { session =>
+        session.execute("DROP TABLE IF EXISTS " + cassandra_keyspace_name + "." + table_idx + ";")
+
+      }
+    }
+    catch {
+      case e: Exception =>
+        System.out.println("A problem occurred dropping the tables")
+        e.printStackTrace()
+        //Stop Spark
+        spark.close()
+        System.exit(1)
+    }
+  }
+
+
+  def writeTableSequenceIndex(combinations: RDD[SetCInverted], logName: String): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    val table_idx = logName + "_idx"
+    val table = combinations
+      .map(r => {
+        val formatted = cassandraFormat(r)
+        Structs.CassandraIndex(formatted._1, formatted._2, formatted._3)
+      })
+    val writeConf = WriteConf(consistencyLevel = ConsistencyLevel.ONE)
+    table.saveToCassandra(
+      keyspaceName = this.cassandra_keyspace_name.toLowerCase(),
+      tableName = table_idx.toLowerCase,
+      columns = SomeColumns(
+        "event1_name",
+        "event2_name",
+        "sequences" append
+      ), writeConf
+    )
+  }
+
+  def cassandraFormat(line:SetCInverted):(String, String, List[String])={
+    (line.event1, line.event2, line.ids.map(x=>x.toString))
+  }
+
+  def closeSpark(): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    println("Closing Spark")
+    spark.close()
+
+  }
+
+
+}
---
+++ src/main/scala/auth/datalab/sequenceDetection/SetContainment/SetContainment.scala
@@ -0,0 +1,46 @@
+package auth.datalab.sequenceDetection.SetContainment
+import auth.datalab.sequenceDetection.PairExtraction.{Indexing, Parsing, SkipTillAnyMatch, State, StrictContiguity}
+import auth.datalab.sequenceDetection.SetContainment.CassandraSetContainment
+import auth.datalab.sequenceDetection.{CassandraConnection, Structs, Utils}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
+
+object SetContainment {
+  case class SetCInverted(event1: String, event2: String, ids: List[Long])
+  private var cassandraConnection: CassandraConnection = null
+
+  def main(args: Array[String]): Unit = {
+    val fileName: String = args(0)
+    val type_of_algorithm = args(1) //parsing, indexing or state
+    val deleteAll = args(2)
+    val join = args(3).toInt
+    val deletePrevious = args(4)
+    println(fileName, type_of_algorithm, deleteAll, join)
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val cassandraConnection = new CassandraSetContainment()
+    cassandraConnection.startSpark()
+    val sequencesRDD: RDD[Structs.Sequence] = Utils.readLog(fileName)
+    val inverted_index = createCombinationsRDD(sequencesRDD,type_of_algorithm)
+
+  }
+
+
+  def createCombinationsRDD(seqRDD: RDD[Structs.Sequence], type_of_algorithm: String): RDD[SetCInverted] = {
+    val combinations = type_of_algorithm match {
+      case "parsing" => Parsing.extract(seqRDD)
+      case "indexing" => Indexing.extract(seqRDD)
+      case "state" => State.extract(seqRDD)
+      case "strict" =>StrictContiguity.extract(seqRDD)
+      case "anymatch" => SkipTillAnyMatch.extract(seqRDD)
+      case _ => throw new Exception("Wrong type of algorithm")
+    }
+    combinations.map(pair=>{
+      val l= pair.times.map(_.id).distinct.sortWith((x,y)=>x<y)
+      SetCInverted(pair.event1,pair.event2,l)
+    })
+
+  }
+
+
+}
