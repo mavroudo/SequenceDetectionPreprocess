2910fc69e44563d99e4c9f9a5e9cc047085aece4
Giannis Mavroudopoulos <mavroudo@csd.auth.gr>
2021-08-28 12:07:26 +0300

    added execution time

--- src/main/scala/auth/datalab/sequenceDetection/SetContainment/SetContainment.scala
+++ src/main/scala/auth/datalab/sequenceDetection/SetContainment/SetContainment.scala
@@ -3,6 +3,7 @@ import auth.datalab.sequenceDetection.PairExtraction.{Indexing, Parsing, SkipTil
 import auth.datalab.sequenceDetection.{Structs, Utils}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
 
 object SetContainment {
@@ -26,14 +27,32 @@ object SetContainment {
       cassandraConnection.dropTable(logName)
     }
     cassandraConnection.createTable(logName)
-    val sequencesRDD: RDD[Structs.Sequence] = Utils.readLog(fileName)
-    sequencesRDD.persist(StorageLevel.MEMORY_AND_DISK)
-    val inverted_index = createCombinationsRDD(sequencesRDD,type_of_algorithm)
-    cassandraConnection.writeTableSequenceIndex(inverted_index,logName)
-    cassandraConnection.writeTableSeq(sequencesRDD,logName)
-    inverted_index.unpersist()
-    sequencesRDD.unpersist()
-    cassandraConnection.closeSpark()
+    try {
+      val spark = SparkSession.builder().getOrCreate()
+      spark.time({
+        val sequencesRDD: RDD[Structs.Sequence] = Utils.readLog(fileName)
+        sequencesRDD.persist(StorageLevel.MEMORY_AND_DISK)
+        val inverted_index = createCombinationsRDD(sequencesRDD, type_of_algorithm)
+        cassandraConnection.writeTableSequenceIndex(inverted_index, logName)
+        cassandraConnection.writeTableSeq(sequencesRDD, logName)
+        inverted_index.unpersist()
+        sequencesRDD.unpersist()
+        cassandraConnection.closeSpark()
+      })
+      val mb = 1024 * 1024
+      val runtime = Runtime.getRuntime
+      println("ALL RESULTS IN MB")
+      println("** Used Memory:  " + (runtime.totalMemory - runtime.freeMemory) / mb)
+      println("** Free Memory:  " + runtime.freeMemory / mb)
+      println("** Total Memory: " + runtime.totalMemory / mb)
+      println("** Max Memory:   " + runtime.maxMemory / mb)
+    }catch {
+      case e: Exception => {
+        e.getStackTrace.foreach(println)
+        println(e.getMessage())
+        cassandraConnection.closeSpark()
+      }
+    }
   }
 
 
