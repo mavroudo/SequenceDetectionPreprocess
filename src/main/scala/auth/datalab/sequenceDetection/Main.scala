9614f5edc384844a7e3d2ce627d6bd8b6cfb906c
mavroudo <mavroudo@csd.auth.gr>
2021-09-14 16:10:40 +0300

    working data generator sclala

--- src/main/scala/auth/datalab/sequenceDetection/Main.scala
+++ src/main/scala/auth/datalab/sequenceDetection/Main.scala
@@ -1,17 +1,28 @@
 package auth.datalab.sequenceDetection
 import auth.datalab.sequenceDetection.Signatures.Signature
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 object Main {
   def main(args: Array[String]): Unit = {
+    val configuration = new SparkConf()
+      .setAppName("FA Indexing")
+      .setMaster("local[*]")
 
-    if(args(5)=="normal"){
-      SequenceDetection.main(args)
-    }else if(args(5)=="signature"){
-      Signature.main(args)
-    }else if(args(5)=="setcontainment"){
-      SetContainment.SetContainment.main(args)
-    }else{
-      println("not a valid choice")
-    }
+
+    val spark = SparkSession.builder().config(configuration).getOrCreate()
+
+    val tg:TraceGenerator = new TraceGenerator(10000,1000,5000,15000)
+    println(tg.produce().count())
+
+//    if(args(5)=="normal"){
+//      SequenceDetection.main(args)
+//    }else if(args(5)=="signature"){
+//      Signature.main(args)
+//    }else if(args(5)=="setcontainment"){
+//      SetContainment.SetContainment.main(args)
+//    }else{
+//      println("not a valid choice")
+//    }
   }
 
 }
