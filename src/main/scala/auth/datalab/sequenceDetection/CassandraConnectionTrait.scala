d2594e67de3ad8d66ceed23f524498ebec900d40
mavroudo <mavroudo@csd.auth.gr>
2022-01-27 14:06:11 +0200

    Fix bug where sequence table had null values after the first iteration

--- src/main/scala/auth/datalab/sequenceDetection/CassandraConnectionTrait.scala
+++ src/main/scala/auth/datalab/sequenceDetection/CassandraConnectionTrait.scala
@@ -43,7 +43,7 @@ trait CassandraConnectionTrait {
     }
     _configuration = new SparkConf()
       .setAppName("FA Indexing")
-      .setMaster("local[*]")
+//      .setMaster("local[*]")
       .set("spark.cassandra.connection.host", cassandra_host)
       .set("spark.cassandra.auth.username", cassandra_user)
       .set("spark.cassandra.auth.password", cassandra_pass)
