4060497b4b8cdc82bcb40fa2ec71b23e325d301f
Ioannis Mavroudopoulos <mavroudo@csd.auth.gr>
2022-03-08 14:48:13 +0200

    Uploading triplets creation

---
+++ src/main/scala/auth/datalab/sequenceDetection/Triplets/CassandraTriplets.scala
@@ -0,0 +1,114 @@
+package auth.datalab.sequenceDetection.Triplets
+
+import auth.datalab.sequenceDetection.{CassandraConnectionTrait, Structs}
+import com.datastax.spark.connector._
+import com.datastax.spark.connector.cql.CassandraConnector
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+
+class CassandraTriplets extends Serializable with CassandraConnectionTrait{
+
+  def createTable(logName: String): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    val table_temp = logName + "_temp"
+    val table_seq = logName + "_seq"
+    val table_idx = logName + "_idx"
+    val table_count = logName + "_count"
+    val tables: Map[String, String] = Map(
+      table_idx -> "event1_name text, event2_name text, event3_name text, sequences list<text>, PRIMARY KEY (event1_name, event2_name, event3_name)",
+      table_temp -> "event1_name text, event2_name text, event3_name text,  sequences list<text>, PRIMARY KEY (event1_name, event2_name, event3_name)",
+      table_count -> "event1_name text,event2_name, sequences_per_field list<text>, PRIMARY KEY (event1_name,event2_name)",
+      table_seq -> "sequence_id text, events list<text>, PRIMARY KEY (sequence_id)"
+    )
+    try {
+      CassandraConnector(spark.sparkContext.getConf).withSessionDo { session =>
+        for (table <- tables) {
+          session.execute("CREATE TABLE IF NOT EXISTS " + cassandra_keyspace_name + "." +
+            table._1 + " (" + table._2 + ") " +
+            "WITH GC_GRACE_SECONDS=" + cassandra_gc_grace_seconds +
+            ";")
+        }
+      }
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        System.out.println("A problem occurred creating the table")
+        //Stop Spark
+        spark.close()
+        System.exit(1)
+    }
+  }
+
+  def dropTable(logName: String): Unit = {
+    val spark = SparkSession.builder().getOrCreate()
+    val table_temp = logName + "_temp"
+    val table_seq = logName + "_seq"
+    val table_idx = logName + "_idx"
+    val table_count = logName + "_count"
+    try {
+      CassandraConnector(spark.sparkContext.getConf).withSessionDo { session =>
+        session.execute("DROP TABLE IF EXISTS " + cassandra_keyspace_name + "." + table_temp + ";")
+        session.execute("DROP TABLE IF EXISTS " + cassandra_keyspace_name + "." + table_seq + ";")
+        session.execute("DROP TABLE IF EXISTS " + cassandra_keyspace_name + "." + table_idx + ";")
+        session.execute("DROP TABLE IF EXISTS " + cassandra_keyspace_name + "." + table_count + ";")
+      }
+    }
+    catch {
+      case e: Exception =>
+        System.out.println("A problem occurred dropping the tables")
+        e.printStackTrace()
+        //Stop Spark
+        spark.close()
+        System.exit(1)
+    }
+  }
+
+  def writeTableSeq(table: RDD[Structs.Sequence], logName: String): Unit ={
+    val table_seq = logName + "_seq"
+    table.filter(_.events.nonEmpty).saveToCassandra(keyspaceName = this.cassandra_keyspace_name.toLowerCase, tableName = table_seq.toLowerCase(),columns = SomeColumns("events","sequence_id"), writeConf = writeConf)
+  }
+
+  def writeTableSequenceIndex(combinations: RDD[Structs.Triplet], logName: String): Unit = {
+    val table_idx = logName + "_idx"
+    val spark = SparkSession.builder().getOrCreate()
+    val table = combinations
+      .map(r => {
+        val formatted = combinationsToCassandraFormat(r)
+        Structs.CassandraIndexTriplets(formatted._1, formatted._2, formatted._3, formatted._4)
+      })
+    //    val writeConf = WriteConf(consistencyLevel = ConsistencyLevel.ONE)
+    table.saveToCassandra(
+      keyspaceName = this.cassandra_keyspace_name.toLowerCase(),
+      tableName = table_idx.toLowerCase,
+      columns = SomeColumns(
+        "event1_name",
+        "event2_name",
+        "event3_name",
+        "sequences" append //Method to append to a list in cassandra
+      ), writeConf
+    )
+  }
+
+  private def combinationsToCassandraFormat(line: Structs.Triplet): (String, String, String, List[String]) = {
+    val newList = line.times
+      .map(r => {
+        var userString = r.id + "("
+        for (i <- 2 until r.times.length by 3) {
+          userString = userString + "(" + r.times(i - 2) + "," + r.times(i-1) + "," + r.times(i)+ "),"
+        }
+        userString = userString.dropRight(1) + ")"
+        userString
+      })
+    (line.event1, line.event2,line.event3, newList)
+  }
+
+//  private def combinationsCountToCassandraFormat(line: Structs.TripleCountList): (String, String, List[String]) = {
+//    val newEvent = line.event1_name
+//    val newList = line.times
+//      .map(r => {
+//        val userString = r._1 +DELIMITER + r._2 + DELIMITER + r._3
+//        userString
+//      })
+//    (newEvent, newList)
+//  }
+}
---
+++ src/main/scala/auth/datalab/sequenceDetection/Triplets/ExtractTriplets.scala
@@ -0,0 +1,134 @@
+package auth.datalab.sequenceDetection.Triplets
+
+import auth.datalab.sequenceDetection.SetContainment.SetContainment.cassandraConnection
+import auth.datalab.sequenceDetection.Structs.Triplet
+import auth.datalab.sequenceDetection.{Structs, Utils}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.storage.StorageLevel
+
+import scala.collection.mutable.{HashMap, ListBuffer}
+
+
+
+object ExtractTriplets {
+
+  private var cassandraConnection: CassandraTriplets = null
+
+  def extract(data: RDD[Structs.Sequence]): RDD[Structs.Triplet] = {
+    val spark = SparkSession.builder().getOrCreate()
+    val combinations = data
+      .flatMap(l => this.indexingMethodTriplets(l))
+      .keyBy(l => (l.event1, l.event2, l.event3))
+      .reduceByKey((a, b) => {
+        val newList = List.concat(a.times, b.times)
+        Structs.Triplet(a.event1, a.event2, a.event3, newList)
+      })
+      .map(_._2)
+      .coalesce(spark.sparkContext.defaultParallelism)
+    combinations
+  }
+
+  private def indexingMethodTriplets(line: Structs.Sequence): List[Triplet] = {
+    var mapping = HashMap[String, List[String]]()
+    val sequence_id = line.sequence_id
+    line.events.foreach(event => { //This will do the mapping
+      val oldSequence = mapping.getOrElse(event.event, null)
+      if (oldSequence == null) {
+        mapping.+=((event.event, List(event.timestamp)))
+      } else {
+        val newList = oldSequence :+ event.timestamp
+        mapping.+=((event.event, newList))
+      }
+    })
+    val triplets = new ListBuffer[Triplet]
+    for((k1,v1) <- mapping){
+      for((k2,v2) <- mapping){
+        for((k3,v3) <- mapping){
+          val list = List[Structs.IdTimeList]() :+ this.createTripletsIndexing(v1, v2, v3, sequence_id)
+          triplets += Triplet(k1, k2, k3, list)
+        }
+      }
+    }
+    val l =triplets.filter(x => {
+      x.times.head.times.nonEmpty
+    })
+    l.toList
+  }
+
+  private def createTripletsIndexing(eventAtimes: List[String], eventBtimes: List[String], eventCtimes: List[String], sequenceid: Long): Structs.IdTimeList = {
+    var posA = 0
+    var posB = 0
+    var posC = 0
+    var prev = ""
+
+    val sortedA = eventAtimes.sortWith(Utils.compareTimes)
+    val sortedB = eventBtimes.sortWith(Utils.compareTimes)
+    val sortedC = eventCtimes.sortWith(Utils.compareTimes)
+
+    var response = Structs.IdTimeList(sequenceid, List[String]())
+
+    while (posA < sortedA.size && posB < sortedB.size && posC < sortedC.size) {
+      if (Utils.compareTimes(sortedA(posA), sortedB(posB)) && Utils.compareTimes(sortedB(posB), sortedC(posC))) { // goes in if (a b c)
+        if (Utils.compareTimes(prev, sortedA(posA))) {
+          val newList = response.times :+ sortedA(posA) :+ sortedB(posB) :+ sortedC(posC)
+          response = Structs.IdTimeList(response.id, newList)
+          prev = sortedC(posC)
+          posA += 1
+          posB += 1
+          posC += 1
+        } else {
+          posA += 1
+        }
+      } else {
+        if (!Utils.compareTimes(sortedA(posA), sortedB(posB))) {
+          posB += 1
+        } else if (!Utils.compareTimes(sortedB(posB), sortedC(posC))) {
+          posC += 1
+        }
+      }
+    }
+    response
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val fileName: String = args(0)
+    val type_of_algorithm = args(1) //parsing, indexing or state
+    val deleteAll = args(2)
+    val join = args(3).toInt
+    val deletePrevious = args(4)
+    println(fileName, type_of_algorithm, deleteAll, join)
+    var logName = fileName.split('/').last.toLowerCase().split('.')(0).split('$')(0).replace(' ', '_')
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    cassandraConnection = new CassandraTriplets()
+    cassandraConnection.startSpark()
+    if (deletePrevious == "1" || deleteAll == "1") {
+      cassandraConnection.dropTable(logName)
+    }
+    cassandraConnection.createTable(logName)
+    try {
+      val spark = SparkSession.builder().getOrCreate()
+      spark.time({
+        val sequencesRDD: RDD[Structs.Sequence] = Utils.readLog(fileName)
+        sequencesRDD.persist(StorageLevel.MEMORY_AND_DISK)
+        cassandraConnection.writeTableSeq(sequencesRDD, logName)
+        val combinationsRDD= extract(sequencesRDD)
+        combinationsRDD.persist(StorageLevel.MEMORY_AND_DISK)
+        sequencesRDD.unpersist()
+        cassandraConnection.writeTableSequenceIndex(combinationsRDD,logName)
+        combinationsRDD.unpersist()
+      })
+    } catch {
+      case e: Exception => {
+        e.getStackTrace.foreach(println)
+        println(e.getMessage)
+        cassandraConnection.closeSpark()
+      }
+
+    }
+  }
+
+}
