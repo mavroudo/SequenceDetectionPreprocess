5f4fd319f0c5564e52e863fb72e8bf5dc351f16e
mavroudo <mavroudo@csd.auth.gr>
2022-05-17 11:55:27 +0300

    Fix bug where sequence table had null values after the first iteration

--- src/main/scala/auth/datalab/sequenceDetection/PairExtraction/Indexing.scala
+++ src/main/scala/auth/datalab/sequenceDetection/PairExtraction/Indexing.scala
@@ -1,12 +1,11 @@
 package auth.datalab.sequenceDetection.PairExtraction
 
 import java.util.Collections
-
 import auth.datalab.sequenceDetection.{Structs, Utils}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
-import scala.collection.mutable.HashMap
+import scala.collection.mutable.{HashMap, ListBuffer}
 
 /**
  * Extract pairs using Indexing method
@@ -16,7 +15,7 @@ object Indexing extends ExtractPairs {
   override def extract(data: RDD[Structs.Sequence]): RDD[Structs.EventIdTimeLists] = {
     val spark = SparkSession.builder().getOrCreate()
     val combinations = data
-      .flatMap(l => this.indexingMethodPairs(l))
+      .flatMap(l => this.indexingMethodPairs2(l))
       .keyBy(l => (l.event1, l.event2))
       .reduceByKey((a, b) => {
         val newList = List.concat(a.times, b.times)
@@ -43,11 +42,28 @@ object Indexing extends ExtractPairs {
       mapping.map(eventB => {
         val list = List[Structs.IdTimeList]() :+ this.createPairsIndexing(eventA._2, eventB._2, sequence_id)
         Structs.EventIdTimeLists(eventA._1, eventB._1, list)
-      }).filter(x =>{
+      }).filter(x => {
         x.times.head.times.nonEmpty
       })
-    })toList
+    }) toList
+  }
 
+  private def indexingMethodPairs2(line: Structs.Sequence): List[Structs.EventIdTimeLists] = {
+    //      var mapping = HashMap[String, List[String]]()
+    val sequence_id = line.sequence_id
+    var mapping = line.events
+      .groupBy(_.event)
+      .map(x => {
+        (x._1, x._2.map(_.timestamp).sortWith(Utils.compareTimes))
+      })
+    val l: ListBuffer[Structs.EventIdTimeLists] = new ListBuffer[Structs.EventIdTimeLists]()
+    mapping.foreach(x1 => {
+      mapping.foreach(x2 => {
+        val list = List[Structs.IdTimeList]() :+ this.createPairsIndexing(x1._2, x2._2, sequence_id)
+        l += Structs.EventIdTimeLists(x1._1, x2._1, list)
+      })
+    })
+    l.toList.filter(_.times.head.times.nonEmpty)
   }
 
   private def createPairsIndexing(eventAtimes: List[String], eventBtimes: List[String], sequenceid: Long): Structs.IdTimeList = {
@@ -55,8 +71,8 @@ object Indexing extends ExtractPairs {
     var posB = 0
     var prev = ""
 
-    val sortedA=eventAtimes.sortWith(Utils.compareTimes)
-    val sortedB=eventBtimes.sortWith(Utils.compareTimes)
+    val sortedA = eventAtimes.sortWith(Utils.compareTimes)
+    val sortedB = eventBtimes.sortWith(Utils.compareTimes)
     var response = Structs.IdTimeList(sequenceid, List[String]())
     while (posA < sortedA.size && posB < sortedB.size) {
       if (Utils.compareTimes(sortedA(posA), sortedB(posB))) { // goes in if a  b
