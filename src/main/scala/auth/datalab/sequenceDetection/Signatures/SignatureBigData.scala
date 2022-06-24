36a30a4a44f06746d1c99db8a6526f3c7acdde96
mavroudo <mavroudo@csd.auth.gr>
2022-06-24 14:47:10 +0300

    Fixing signature to handle 10.000 traces per time

--- src/main/scala/auth/datalab/sequenceDetection/Signatures/SignatureBigData.scala
+++ src/main/scala/auth/datalab/sequenceDetection/Signatures/SignatureBigData.scala
@@ -1,13 +1,16 @@
 package auth.datalab.sequenceDetection.Signatures
 
 import auth.datalab.sequenceDetection.SetContainment.SetcontainmentBigData.cassandraConnection
-import auth.datalab.sequenceDetection.Signatures.Signature.{Signatures, createPairs, createSignature}
+import auth.datalab.sequenceDetection.Signatures.Signature.{Signature, Signatures, createPairs, createSignature}
 import auth.datalab.sequenceDetection.{Structs, TraceGenerator}
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.util.SizeEstimator
+
+import scala.collection.mutable
 
 object SignatureBigData {
 
@@ -39,21 +42,22 @@ object SignatureBigData {
       val size_estimate_trace: scala.math.BigInt = SizeEstimator.estimate(traceGenerator.estimate_size().events.head) * traceGenerator.maxTraceSize
       var partitionNumber = if (minExecutorMemory >= size_estimate_trace * traces) 0 else ((size_estimate_trace * traces) / minExecutorMemory).toInt + 1
       partitionNumber = partitionNumber / allExecutors + 2
-      val ids = (1 to traces).toList.sliding(5000, 5000).toList
+      val ids = (1 to traces).toList.sliding(50000, 50000).toList
       println("Iterations: ", ids.length)
 
-      val startRDD=traceGenerator.produce((1 to 1000).toList)
+      val startRDD = traceGenerator.produce((1 to 1000).toList)
       val topKfreqPairs: List[(String, String)] = startRDD.map(createPairs).flatMap(x => x.pairs)
-          .map(x => ((x.eventA, x.eventB), 1))
-          .reduceByKey(_ + _)
-          .sortBy(_._2, false)
-          .take(k)
-          .map(_._1)
-          .toList
+        .map(x => ((x.eventA, x.eventB), 1))
+        .reduceByKey(_ + _)
+        .sortBy(_._2, false)
+        .take(k)
+        .map(_._1)
+        .toList
       val events: List[String] = startRDD.flatMap(_.events).map(_.event).distinct().collect.toList
       cassandraConnection.writeTableMetadata(events, topKfreqPairs, logName)
       println("Metadata saved ...")
-
+      val bPairs = spark.sparkContext.broadcast(topKfreqPairs)
+      val bEvents = spark.sparkContext.broadcast(events)
       var t = 0L
       for (id <- ids) {
         println(id.head, id.last)
@@ -62,12 +66,13 @@ object SignatureBigData {
         val start = System.currentTimeMillis()
         k = sequencesRDD.flatMap(x => x.events).map(_.event).distinct.count().toInt
         cassandraConnection.writeTableSeq(sequencesRDD, logName)
-        val signatures = sequencesRDD.map(x => createSignature(x, events, topKfreqPairs))
+        val signatures = sequencesRDD.map(x => createBSignature(x, bEvents, bPairs))
           .groupBy(_.signature)
           .map(x => Signatures(x._1, x._2.map(_.sequence_id.toString).toList))
-        sequencesRDD.unpersist()
+
         signatures.persist(StorageLevel.MEMORY_AND_DISK)
         cassandraConnection.writeTableSign(signatures, logName)
+        sequencesRDD.unpersist()
         signatures.unpersist()
         t = t + System.currentTimeMillis() - start
 
@@ -90,7 +95,19 @@ object SignatureBigData {
       }
     }
 
+  }
 
+  def createBSignature(sequence: Structs.Sequence, events: Broadcast[List[String]], topKfreqPairs: Broadcast[List[(String, String)]]): Signature = {
+    val s = new mutable.StringBuilder()
+    val containedEvents = sequence.events.map(_.event).distinct
+    for (event <- events.value) {
+      if (containedEvents.contains(event)) s += '1' else s += '0'
+    }
+    val pairs = createPairs(sequence)
+    for (freqPair <- topKfreqPairs.value) {
+      if (pairs.pairs.map(x => (x.eventA, x.eventB)).contains(freqPair)) s += '1' else s += '0'
+    }
+    new Signature(s.toString(), sequence.sequence_id)
   }
 
 }
