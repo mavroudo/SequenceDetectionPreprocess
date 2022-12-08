f1b1cc6317b0ea5fc59c9d1bf8ebf4471f0e132a
mavroudo <mavroudo@csd.auth.gr>
2022-12-08 16:52:26 +0200

    Generate pairs is working as was supposed

--- src/test/scala/Logic/TestExtractPairs.scala
+++ src/test/scala/Logic/TestExtractPairs.scala
@@ -93,6 +93,119 @@ class TestExtractPairs extends FunSuite with BeforeAndAfterAll{
     assert(pairs_c.count(x=>x.eventA=="a" && x.eventB=="c")==1)
   }
 
+  test("split every 30 days - lookback 30 (2)") {
+    config = Config(delete_previous = true, log_name = "test", split_every_days = 30)
+    dbConnector.initialize_spark(config)
+    val spark = SparkSession.builder().getOrCreate()
+    this.dbConnector.initialize_db(config)
+    this.metaData = dbConnector.get_metadata(config)
+    val data = spark.sparkContext.parallelize(CreateRDD.createRDD_1)
+    dbConnector.write_sequence_table(data, metaData)
+    val intervals = Intervals.intervals(data, "", config.split_every_days)
+    metaData.last_interval = s"${intervals.last.start.toString}_${intervals.last.end.toString}"
+    val invertedSingleFull = ExtractSingle.extractFull(data)
+    val combinedInvertedFull = dbConnector.write_single_table(invertedSingleFull, metaData)
+    val x = ExtractPairs.extract(combinedInvertedFull, null, intervals, config.lookback_days)
+    dbConnector.write_last_checked_table(x._2, metaData)
+    dbConnector.write_index_table(x._1, metaData, intervals)
+    val counts = ExtractCounts.extract(x._1)
+    dbConnector.write_count_table(counts, metaData)
+    dbConnector.write_metadata(metaData) //till here index the first one
+
+    //index the second one
+    val data2 = spark.sparkContext.parallelize(CreateRDD.createRDD_2)
+    val d = dbConnector.write_sequence_table(data2, metaData)
+    val invertedSingleFull2 = ExtractSingle.extractFull(d)
+    val intervals2 = Intervals.intervals(data2, metaData.last_interval, config.split_every_days)
+    val combinedInvertedFull2 = dbConnector.write_single_table(invertedSingleFull2, metaData)
+    val last_checked = dbConnector.read_last_checked_table(metaData)
+    val x2 = ExtractPairs.extract(combinedInvertedFull2, last_checked, intervals2, config.lookback_days)
+    val pairs_c = x2._1.collect()
+    val last_checked_c = x2._2.collect()
+    assert(pairs_c.length == 9)
+    assert(last_checked_c.length == 9)
+    assert(pairs_c.count(x => x.eventA == "a" && x.eventB == "a") == 2)
+    assert(pairs_c.count(x => x.eventA == "c" && x.eventB == "a") == 2)
+    assert(pairs_c.count(x => x.eventA == "b" && x.eventB == "a") == 1)
+    assert(pairs_c.count(x => x.eventA == "a" && x.eventB == "b") == 1)
+    assert(pairs_c.count(x => x.eventA == "c" && x.eventB == "c") == 1)
+    assert(pairs_c.count(x => x.eventA == "b" && x.eventB == "c") == 1)
+    assert(pairs_c.count(x => x.eventA == "a" && x.eventB == "c") == 1)
+  }
+
+  test("split every 30 days - lookback 10 (2)") {
+    config = Config(delete_previous = true, log_name = "test", lookback_days = 10)
+    dbConnector.initialize_spark(config)
+    val spark = SparkSession.builder().getOrCreate()
+    this.dbConnector.initialize_db(config)
+    this.metaData = dbConnector.get_metadata(config)
+    val data = spark.sparkContext.parallelize(CreateRDD.createRDD_1)
+    dbConnector.write_sequence_table(data, metaData)
+    val intervals = Intervals.intervals(data, "", config.split_every_days)
+    metaData.last_interval = s"${intervals.last.start.toString}_${intervals.last.end.toString}"
+    val invertedSingleFull = ExtractSingle.extractFull(data)
+    val combinedInvertedFull = dbConnector.write_single_table(invertedSingleFull, metaData)
+    val x = ExtractPairs.extract(combinedInvertedFull, null, intervals, config.lookback_days)
+    dbConnector.write_last_checked_table(x._2, metaData)
+    dbConnector.write_index_table(x._1, metaData, intervals)
+    val counts = ExtractCounts.extract(x._1)
+    dbConnector.write_count_table(counts, metaData)
+    dbConnector.write_metadata(metaData) //till here index the first one
+
+    //index the second one
+    val data2 = spark.sparkContext.parallelize(CreateRDD.createRDD_2)
+    val d = dbConnector.write_sequence_table(data2, metaData)
+    val invertedSingleFull2 = ExtractSingle.extractFull(d)
+    val intervals2 = Intervals.intervals(data2, metaData.last_interval, config.split_every_days)
+    val combinedInvertedFull2 = dbConnector.write_single_table(invertedSingleFull2, metaData)
+    val last_checked = dbConnector.read_last_checked_table(metaData)
+    val x2 = ExtractPairs.extract(combinedInvertedFull2, last_checked, intervals2, config.lookback_days)
+    val pairs_c = x2._1.collect()
+    val last_checked_c = x2._2.collect()
+    assert(pairs_c.length == 4)
+    assert(last_checked_c.length == 4)
+    assert(pairs_c.count(x => x.eventA == "a" && x.eventB == "a") == 1)
+    assert(pairs_c.count(x => x.eventA == "c" && x.eventB == "a") == 2)
+    assert(pairs_c.count(x => x.eventA == "a" && x.eventB == "b") == 1)
+  }
+
+  test("split every 30 days - lookback 15 (2)") {
+    config = Config(delete_previous = true, log_name = "test", lookback_days = 15)
+    dbConnector.initialize_spark(config)
+    val spark = SparkSession.builder().getOrCreate()
+    this.dbConnector.initialize_db(config)
+    this.metaData = dbConnector.get_metadata(config)
+    val data = spark.sparkContext.parallelize(CreateRDD.createRDD_1)
+    dbConnector.write_sequence_table(data, metaData)
+    val intervals = Intervals.intervals(data, "", config.split_every_days)
+    metaData.last_interval = s"${intervals.last.start.toString}_${intervals.last.end.toString}"
+    val invertedSingleFull = ExtractSingle.extractFull(data)
+    val combinedInvertedFull = dbConnector.write_single_table(invertedSingleFull, metaData)
+    val x = ExtractPairs.extract(combinedInvertedFull, null, intervals, config.lookback_days)
+    dbConnector.write_last_checked_table(x._2, metaData)
+    dbConnector.write_index_table(x._1, metaData, intervals)
+    val counts = ExtractCounts.extract(x._1)
+    dbConnector.write_count_table(counts, metaData)
+    dbConnector.write_metadata(metaData) //till here index the first one
+
+    //index the second one
+    val data2 = spark.sparkContext.parallelize(CreateRDD.createRDD_2)
+    val d = dbConnector.write_sequence_table(data2, metaData)
+    val invertedSingleFull2 = ExtractSingle.extractFull(d)
+    val intervals2 = Intervals.intervals(data2, metaData.last_interval, config.split_every_days)
+    val combinedInvertedFull2 = dbConnector.write_single_table(invertedSingleFull2, metaData)
+    val last_checked = dbConnector.read_last_checked_table(metaData)
+    val x2 = ExtractPairs.extract(combinedInvertedFull2, last_checked, intervals2, config.lookback_days)
+    val pairs_c = x2._1.collect()
+    val last_checked_c = x2._2.collect()
+    assert(pairs_c.length == 5)
+    assert(last_checked_c.length == 5)
+    assert(pairs_c.count(x => x.eventA == "a" && x.eventB == "a") == 1)
+    assert(pairs_c.count(x => x.eventA == "c" && x.eventB == "a") == 2)
+    assert(pairs_c.count(x => x.eventA == "b" && x.eventB == "a") == 1)
+    assert(pairs_c.count(x => x.eventA == "a" && x.eventB == "b") == 1)
+  }
+
   override def afterAll(): Unit = {
     this.dbConnector.closeSpark()
   }
