43a6085f17d0c8258400d60f0bd789f78ac2c890
Ioannis Mavroudopoulos <mavroudo@csd.auth.gr>
2022-12-11 11:14:33 +0200

    Remove all the previous implementations

--- src/main/scala/auth/datalab/sequenceDetection/PairExtraction/ExtractPairs.scala
+++
@@ -1,8 +0,0 @@
-package auth.datalab.sequenceDetection.PairExtraction
-
-import auth.datalab.sequenceDetection.Structs
-import org.apache.spark.rdd.RDD
-
-trait ExtractPairs {
-    def extract(data:RDD[Structs.Sequence]):RDD[Structs.EventIdTimeLists]
-}
