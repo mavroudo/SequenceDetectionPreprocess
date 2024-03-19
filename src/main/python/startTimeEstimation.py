259d8db4ed8b1e46ae5b3320372276a1d9e351c0
Christos Balaktsis <mpalaktsc@csd.auth.gr>
2024-03-19 13:01:19 +0200

    Implemented resource availability normalization

--- src/main/python/startTimeEstimation.py
+++ src/main/python/startTimeEstimation.py
@@ -4,18 +4,24 @@ import sys
 '''
     This script reads a file with sequences of activity instances and their end-time timestamps, and creates a dict with the following schema:
         *   key = trace index: int
-        *   value = array<struct<event_type: str, start_timestamp: datetime, end_timestamp: datetime, resourse: str>>    
+        *   value = array<struct<event_type: str, start_timestamp: datetime, end_timestamp: datetime, resourse: str>>
 
-    The start time of an activity instance (task) is defined as:
-        *   the pre-defined trace start time, if the task is first in the trace.
-        *   the end time of the last previous task in the trace, if the task is not first in the trace and that last previous task is not concurrent with it.
-    
-    Let the start time of trace to be defined as the first task's start time in exact hour. 
+    The start time of an activity instance (task) is determined in two phases:
+        1.  Calculate the enablement time of the tasks based on the concurrency of the tasks.
+            The enablement time of a task is defined as:
+            *   the pre-defined trace start time, if the task is first in the trace.
+            *   the end time of the last previous task in the trace, if the task is not first in the trace and that last previous task
+                is not concurrent with it.
+        2.  Fix the enablement time of the tasks based on the resource availability.
+            The start time of the task is defined as the maximum of: the enablement time of the task, and the end time of the last task
+            of the same resource in the trace.
+
+    Let the start time of trace to be defined as the first task's start time in exact hour.
     E.g. if the first task starts at 10:15, the trace start time will be 10:00.
-        
-    We define concurency of two activity instances (a.k.a. tasks) as described in the paper "Split Miner: 
+
+    We define concurency of two activity instances (a.k.a. tasks) as described in the paper "Split Miner:
     Discovering Accurate and Simple Business Process Models from Event Logs" (Augusto et al. 2017, https://kodu.ut.ee/~dumas/pubs/icdm2017-split-miner.pdf).
-    Tasks A and B are concurrent iff: 
+    Tasks A and B are concurrent iff:
         *   there are 2 traces in log L such that in one trace A is directly followed by B, and in the other trace B is directly followed by A.
         *   there is no trace in log L such that A is directly followed by B and B is directly followed by A.
         *   the ratio (| |A->B| - |B->A| |) / (|A->B| + |B->A|) is less than 1.
@@ -28,7 +34,7 @@ class Task:
         self.end_timestamp: datetime = datetime.strptime(end_timestamp, '%Y-%m-%d %H:%M:%S')
         self.start_timestamp: datetime = datetime.strptime(start_timestamp, '%Y-%m-%d %H:%M:%S') if start_timestamp else None
         self.resource: str = resource
-    
+
     def __str__(self) -> str:
         return f"Task({self.event_type}, {self.start_timestamp if self.start_timestamp else '?'}, {self.end_timestamp}, {self.resource if self.resource else '?'})"
 
@@ -41,18 +47,21 @@ def printTasks(traces, duration=False):
             if duration:
                 print(f"-> {divmod((task.end_timestamp - task.start_timestamp).total_seconds(), 60)[0]} min")
 
+
 def ingest_data(file_name, separator, delimiter):
     # Set of types of activity instances, e.g. "A", "B", "C"
     types = set()
     # Pairs of activity instances, e.g. "A->B", "B->C"
     pairs = dict()
+    # Dict of resources and their tasks
+    resources = dict()
     # Dictionary of traces and their tasks
     traces = {}
-   
+
     with open(file_name, 'r') as reader:
         for line in reader:
             parts = line.strip().split("::")
-            
+
             trace_index = int(parts[0])
             traces[trace_index] = []
 
@@ -61,7 +70,16 @@ def ingest_data(file_name, separator, delimiter):
                 type = task.split(delimiter)[0]
                 types.add(type)
                 end = task.split(delimiter)[1]
-                traces[trace_index].append(Task(type, end))
+                resource = task.split(delimiter)[2]
+
+                task = Task(event_type=type, end_timestamp=end, resource=resource)
+
+                if resources not in resources.keys():
+                    resources[resource] = [task]
+                else:
+                    resources[resource].append(task)
+
+                traces[trace_index].append(task)
 
                 if i < len(tasks) - 1:
                     if type + tasks[i+1].split(delimiter)[0] in pairs.keys():
@@ -69,7 +87,12 @@ def ingest_data(file_name, separator, delimiter):
                     else:
                         pairs[type + tasks[i+1].split(delimiter)[0]] = 1
                         pairs[tasks[i+1].split(delimiter)[0] + type] = 0
-    return traces, pairs, types
+
+    for resource in resources.values():
+        resource.sort(key=lambda x: x.end_timestamp)
+
+    return traces, pairs, types, resources
+
 
 def find_concurrency(traces, pairs, types) -> set:
     non_concurrents = set()
@@ -81,7 +104,7 @@ def find_concurrency(traces, pairs, types) -> set:
         for j in types:
             if i != j:
                 candidates.add(i + j)
-    
+
     # Exonerate certain non-concurrent pairs based on the first condition
     # O(n) but since n is small, it's ok
     for candidate in candidates:
@@ -92,7 +115,7 @@ def find_concurrency(traces, pairs, types) -> set:
             if pairs[candidate] == 0:
                 non_concurrents.add(candidate)
                 non_concurrents.add(candidate[::-1])
-    
+
     # Exonerate certain non-concurrent pairs based on the second condition
     # O(m*n) but since n is small, it's ok
     for tasks in traces.values():
@@ -100,12 +123,12 @@ def find_concurrency(traces, pairs, types) -> set:
             if tasks[i - 1].event_type == tasks[i + 1].event_type:
                 non_concurrents.add(tasks[i-1].event_type + tasks[i].event_type)
                 non_concurrents.add(tasks[i].event_type + tasks[i-1].event_type)
-    
+
     # Clear the candidates set from the non-concurrent pairs
     for nc in non_concurrents:
         if nc in candidates:
             candidates.remove(nc)
-    
+
     # Exonerate certain non-concurrent pairs based on the third condition
     for candidate in candidates:
         if abs(pairs[candidate] - pairs[candidate[::-1]]) / (pairs[candidate] + pairs[candidate[::-1]]) >= 1:
@@ -120,12 +143,12 @@ def find_concurrency(traces, pairs, types) -> set:
     return candidates
 
 
-def estimate_start_times(traces, concurrents):
+def determine_enablement_times(traces, concurrents):
     for trace_index, tasks in traces.items():
         for i, task in enumerate(tasks):
             # Start time policy for the first task of a trace:
             #   *   If the first task's end time is not at the exact hour, then the start time is the exact hour.
-            #   *   If the first task's end time is at the exact hour, then the start time is the exact hour minus one hour.     
+            #   *   If the first task's end time is at the exact hour, then the start time is the exact hour minus one hour.
             if i == 0:
                 if task.end_timestamp.minute != 0 or task.end_timestamp.second != 0:
                     task.start_timestamp = task.end_timestamp.replace(minute=0, second=0, microsecond=0)
@@ -141,6 +164,14 @@ def estimate_start_times(traces, concurrents):
                     task.start_timestamp = tasks[i-1].end_timestamp
 
 
+def fix_start_times(traces, resources):
+    for trace_index, tasks in traces.items():
+        for task in tasks:
+            max_available_time = max([t.end_timestamp for t in resources[task.resource] if t.end_timestamp < task.start_timestamp])
+            if task.start_timestamp < max_available_time:
+                task.start_timestamp = max_available_time
+
+
 if __name__ == '__main__':
     """ Parameters """
     # The path of the log file
@@ -153,14 +184,16 @@ if __name__ == '__main__':
 
     """ Pipeline """
     # Dictionary of traces and their tasks
-    traces , pairs , types = ingest_data(file_name, separator, delimiter)
+    traces , pairs , types, resources = ingest_data(file_name, separator, delimiter)
 
     # Set of concurrent pairs of tasks
     concurrents = find_concurrency(traces, pairs, types)
 
-    # Update the traces by estimating the start time of the tasks
-    estimate_start_times(traces, concurrents)
-
+    # Find the start time of the tasks based on the concurrent activities and the resource availability
+    # Update the traces by estimating the ebablement time of the tasks
+    determine_enablement_times(traces, concurrents)
+    # Fix the start time of the tasks based on the resource availability
+    fix_start_times(traces, resources)
 
     # print(f"Pairs: {pairs}")
     print(f"Types: {types}")
